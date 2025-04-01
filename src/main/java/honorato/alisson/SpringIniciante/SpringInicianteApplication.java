9a858cd6d3f2c6995b82000c31be49d034b80a47
Alisson-Honorato <alissonhonorato58@gmail.com>
2025-03-31 21:29:27 -0300

    primeiro commit

---
+++ src/main/java/honorato/alisson/SpringIniciante/SpringInicianteApplication.java
@@ -0,0 +1,13 @@
+package honorato.alisson.SpringIniciante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInicianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInicianteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/honorato/alisson/SpringIniciante/SpringInicianteApplicationTests.java
@@ -0,0 +1,13 @@
+package honorato.alisson.SpringIniciante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInicianteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
