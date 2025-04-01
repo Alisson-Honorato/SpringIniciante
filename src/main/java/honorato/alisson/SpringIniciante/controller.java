258500d30b6725d8a8ba727bffbe062cb3607246
Alisson-Honorato <alissonhonorato58@gmail.com>
2025-04-01 20:45:53 -0300

    Criado a controller

---
+++ src/main/java/honorato/alisson/SpringIniciante/controller.java
@@ -0,0 +1,21 @@
+package honorato.alisson.SpringIniciante;
+
+
+import java.beans.JavaBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@JavaBean
+
+public class controller {
+
+    @GetMapping("/boasvindas")
+
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota, ai ai essa porta fica dando erro toda hora tenho que ficar matando toda hora o processo da porta 8080 por que fica toda aberta.";
+    }
+
+}
